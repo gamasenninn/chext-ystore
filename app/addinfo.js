565ac80a71e35050039ebf6b8a940625e81b887d
gamasenninn <gamasenninn@yahoo.co.jp>
2022-06-23 14:58:02 +0900

    退避

--- app/addinfo.js
+++ app/addinfo.js
@@ -8,16 +8,17 @@ $(function () {
 
         $('ul').filter('.sc-cJSrbW').each(async function (i, elUl) {
             let scode = $(elUl).find('li').eq(1).find('.sc-iELTvK').text().replace('管理番号', '').trim();
-            $(elUl).find('li').eq(1).find('.sc-iELTvK').html(`ポータルリンク-> <a href="https://hikousen-rs.com/close?scode=${scode}" target="_blank">管理番号 ${scode}</a>`);
-            //$(elUl).find('li').eq(1).find('.sc-iELTvK').html(`ポータルリンク-> <button onclick=window.open("https://hikousen-rs.com/close?scode=${scode}")>${scode}</button>`);
             let aucId = $(elUl).find('li').eq(1).find('.sc-feJyhm').text().replace('オークションID', '').trim();
-            //const response = await fetch(`https://page.auctions.yahoo.co.jp/jp/auction/${aucId}`);
-            //const html = await response.text();   
-            //console.log(scode);
             $(elUl).find('li').eq(7).remove();
             if (i != 0){
-                $(elUl).append("<li><img src='https://hikousen-rs.com/scode/" + scode + ".jpg' width=130></li>");
-                const response = await fetch('https://hikousen-rs.com/scodeinf.php?scode='+scode);
+                $(elUl).append(
+                    `<li><img src='${conf.imageUrl}/${scode}.jpg' width=120>
+                        <p style="font-size:13px;margin:5px">
+                            <a href="${conf.codeInfoUrl}?scode=${scode}" target="_blank">ポータルへ</a>
+                        </p>
+                    </li>`
+                );
+                const response = await fetch(`${conf.codeInfoApiUrl}?scode=${scode}`);
                 const data = await response.json();   
                 //console.log(data,addWatch,addPv); 
                 $(elUl).find('li').eq(3).find('div').html(`${data.watch}`); //読み直さないと値が設定できないみたい。
